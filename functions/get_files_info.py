16ea3785fb39f42681ce35fb110e4572f884b11d
Patrick Joan Thomson <39674144+patrickjoan@users.noreply.github.com>
2025-07-08 21:49:33 +0300

    Refactor: Move function and remove pycache from git

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -24,27 +24,3 @@ def get_files_info(working_directory, directory=None) -> str:
             return f"Error listing files: {e}"
     except Exception as e:
         return f"Error: {e}"
-
-
-def get_file_content(working_directory, file_path) -> str:
-    try:
-        working_dir_abs_path = os.path.abspath(working_directory)
-        file_path = os.path.join(working_dir_abs_path, file_path)
-
-        if not file_path.startswith(working_dir_abs_path):
-            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not Path(file_path).is_file():
-            return f'Error: File not found or is not a regular file: "{file_path}"'
-
-        file_content = ""
-        with open(file_path, "r", encoding="utf-8") as file:
-            try:
-                file_content = file.read()
-            except UnicodeDecodeError:
-                return f'Error: Cannot read "{file_path}" due to encoding issues'
-
-        if len(file_content) > 10000:
-            file_content = file_content[:10000] + f'[...File "{file_path}"'
-        return file_content
-    except Exception as e:
-        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def main():
